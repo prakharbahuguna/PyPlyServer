9bc463cf6bb2e4590a75ecb5e0484fe0708ff7bf
Tom Robinson <shadlington@thd.vg>
2015-04-18 01:44:35 +0100

    Removed the dumb test code

--- src/server.py
+++ src/server.py
@@ -7,19 +7,12 @@ file_handler = RotatingFileHandler("/opt/repo/ROOT/log.txt")
 file_handler.setLevel(logging.WARNING)
 app.logger.addHandler(file_handler)
 
-with open('/opt/repo/ROOT/test1.txt', 'a') as the_file:
-    the_file.write('TEST')
-
 @app.route('/')
 def hello_world():
-    with open('/opt/repo/ROOT/test2.txt', 'a') as the_file:
-        the_file.write('TEST')
     return 'Hello World!'
 
 @app.route('/spotifyToken')
 def spotify_token():
-    with open('/opt/repo/ROOT/test3.txt', 'a') as the_file:
-        the_file.write('TEST')
     return 'TOKEN'
 
 if __name__ == '__main__':
