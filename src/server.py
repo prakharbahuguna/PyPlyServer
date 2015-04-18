78f9a498ae4291f40ad2bd9b9042c62c2940e17f
Matt Akerman <m.ak@me.com>
2015-04-18 02:45:07 +0100

    Spotify token route amended to return control

--- src/server.py
+++ src/server.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import base64
+import json
 import logging
 from logging.handlers import RotatingFileHandler
+import APIUtils
 
 app = Flask(__name__)
 file_handler = RotatingFileHandler("/opt/repo/ROOT/log.txt")
@@ -18,9 +22,28 @@ def hello_world():
 
 @app.route('/spotifyToken')
 def spotify_token():
-    with open('/opt/repo/ROOT/test3.txt', 'a') as the_file:
-        the_file.write('TEST')
-    return 'TOKEN'
+
+    apiUtils = APIUtils()
+
+    # Auth Step 4: Requests refresh and access tokens
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": apiUtils.getSpotifyRedirectURI()
+    }
+    base64encoded = base64.b64encode("{}:{}".format((), apiUtils.getSpotifyClientID(), apiUtils.getSpotifyClientSecret))
+    headers = {"Authorization": "Basic {}".format(base64encoded)}
+    post_request = requests.post(apiUtils.getSpotifyTokenURL(), data=code_payload, headers=headers)
+
+    # Auth Step 5: Tokens are Returned to Application
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    token_type = response_data["token_type"]
+    expires_in = response_data["expires_in"]
+
+    return
 
 if __name__ == '__main__':
     app.run()
