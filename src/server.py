f8aec5c0f7943288ecd68effc4ba85338ac76296
Tom Robinson <shadlington@thd.vg>
2015-04-18 01:34:48 +0100

    dumb test code

--- src/server.py
+++ src/server.py
@@ -7,12 +7,19 @@ file_handler = RotatingFileHandler("/opt/repo/ROOT/log.txt")
 file_handler.setLevel(logging.WARNING)
 app.logger.addHandler(file_handler)
 
+with open('test1.txt', 'a') as the_file:
+    the_file.write('TEST')
+
 @app.route('/')
 def hello_world():
+    with open('test2.txt', 'a') as the_file:
+        the_file.write('TEST')
     return 'Hello World!'
 
 @app.route('/spotifyToken')
 def spotify_token():
+    with open('test3.txt', 'a') as the_file:
+        the_file.write('TEST')
     return 'TOKEN'
 
 if __name__ == '__main__':
