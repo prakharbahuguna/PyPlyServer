d5b17feb288352c63283ccd1c1c107339870c89d
Prakhar Bahuguna <prakharbahuguna@users.noreply.github.com>
2015-07-02 21:28:13 +0100

    Redacted Twilio API key

--- src/twilioBroker.py
+++ src/twilioBroker.py
@@ -3,14 +3,14 @@ from twilio.rest import TwilioRestClient
 
 
 class TwilioBroker:
-    ACCOUNT_SID = "AC3700d9ec5419d1a4405e9e7338a7fd72"
-    AUTH_TOKEN = "38788b60b0b530ad894ca81d77541a96"
+    ACCOUNT_SID = "" # REDACTED
+    AUTH_TOKEN = ""  # REDACTED
     client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
     def playMp3ToUser(self, phoneNumber, trackUrl):
         call = self.client.calls.create(
                 to=phoneNumber,
-                from_="+441163262273",
+                from_="", # REDACTED
                 url=trackUrl,
                 method="GET",
                 fallback_method="GET",
